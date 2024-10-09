51150079327cc8482fed5f96d8d4d7704afddb11
Dhaval Chaudhari <chdhaval@sheridancollege.ca>
2024-10-08 20:48:44 -0400

    completed  CardGame.java

---
+++ src/main/java/ca/sheridancollege/project/Projectdelivered.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author dgcha
+ */
+public class Projectdelivered {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
